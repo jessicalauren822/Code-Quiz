7b4292157218ea0fc72cbc5a81219622584576d6
jessicawright822 <104212202+jessicawright822@users.noreply.github.com>
2022-05-27 20:02:58 -0400

    js updated for determining questions correct or incorrect

--- assets/js/game.js
+++ assets/js/game.js
@@ -40,3 +40,63 @@ let questions = [
         answer: 4,
     }
 ]
+
+var SCORE_POINTS = 100
+var MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+gatNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionsCounter < MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score)
+
+    return window.location.assign("/end.html")
+    }
+
+    questionCounter++
+    progressText.innerText = "Question ${questionCounter} of ${MAX_QUESTIONS}"
+
+    var questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        var number = choice.dataset["number"]
+        choice.innerText = currentQuestion["choice" + number]
+    })
+
+    availableQuestions. splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener("click", event => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        var selectedChoice = event.target
+        var selectedAnswer = selectedChoice.dataset["number"]
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect"
+
+        if(classToApply === "correct") {
+            incrementscore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore
