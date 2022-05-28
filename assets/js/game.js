39557e3c36cfe66eb07faeb7a4b223c1ed99eccb
jessicawright822 <104212202+jessicawright822@users.noreply.github.com>
2022-05-28 07:37:32 -0400

    adjusting index.html

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,108 +1,113 @@
-var question = document.querySelector("#question");
-var choices = Array.from(document.querySelectorAll(".choice-text"));
-var progressText = document.querySelector("#progressText");
-var scoreText = document.querySelector("#score");
 var score = 0;
+var questionCounter = 0;
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let  questionCounter = 0
-let availableQuestions = []
 
-let questions = [
-    {
-        question: "What is 2 + 2?",
-        choice1: "2", 
-        choice2: "4", 
-        choice3: "21", 
-        choice4: "17", 
-        answer: 2,
-    },
-    {
-        question: "What is 3 + 3?",
-        choice1: "6", 
-        choice2: "3", 
-        choice3: "12", 
-        choice4: "9", 
-        answer: 1,
-    },  {
-        question: "What is 4 + 4?",
-        choice1: "10", 
-        choice2: "12", 
-        choice3: "9", 
-        choice4: "8", 
-        answer: 3,
-    },  {
-        question: "What is 5 + 5?",
-        choice1: "11", 
-        choice2: "13", 
-        choice3: "12", 
-        choice4: "10", 
-        answer: 4,
-    }
-]
 
-var SCORE_POINTS = 100
-var MAX_QUESTIONS = 4
+// var question = document.querySelector("#question");
+// var choices = Array.from(document.querySelectorAll(".choice-text"));
+// var progressText = document.querySelector("#progressText");
+// var scoreText = document.querySelector("#score");
+// var score = 0;
 
-startGame = () => {
-    var questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
-}
+// let currentQuestion = {}
+// let acceptingAnswers = true
+// let  questionCounter = 0
+// let availableQuestions = []
 
-getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter < MAX_QUESTIONS) {
-    localStorage.setItem("mostRecentScore", score)
-    createDiv.textContent = "End of quiz!" + " " + "You got " + score + "/" + questions.legth + "Correct!";
-    questionsDiv.appendChild(createDiv);
-}
+// let questions = [
+//     {
+//         question: "What is 2 + 2?",
+//         choice1: "2", 
+//         choice2: "4", 
+//         choice3: "21", 
+//         choice4: "17", 
+//         answer: 2,
+//     },
+//     {
+//         question: "What is 3 + 3?",
+//         choice1: "6", 
+//         choice2: "3", 
+//         choice3: "12", 
+//         choice4: "9", 
+//         answer: 1,
+//     },  {
+//         question: "What is 4 + 4?",
+//         choice1: "10", 
+//         choice2: "12", 
+//         choice3: "9", 
+//         choice4: "8", 
+//         answer: 3,
+//     },  {
+//         question: "What is 5 + 5?",
+//         choice1: "11", 
+//         choice2: "13", 
+//         choice3: "12", 
+//         choice4: "10", 
+//         answer: 4,
+//     }
+// ]
 
-    questionCounter++
-    progressText.innerText = questions (questionCounter); (MAX_QUESTIONS)
+// var SCORE_POINTS = 100
+// var MAX_QUESTIONS = 4
 
-    var questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
+// startGame = () => {
+//     var questionCounter = 0
+//     score = 0
+//     availableQuestions = [...questions]
+//     getNewQuestion()
+// }
 
-    choices.forEach(choice => {
-        var number = choice.dataset["number"]
-        choice.innerText = currentQuestion["choice" + number]
-    })
+// getNewQuestion = () => {
+//     if (availableQuestions.length === 0 || questionCounter < MAX_QUESTIONS) {
+//     localStorage.setItem("mostRecentScore", score)
+//     createDiv.textContent = "End of quiz!" + " " + "You got " + score + "/" + questions.legth + "Correct!";
+//     questionsDiv.appendChild(createDiv);
+// }
 
-    availableQuestions. splice(questionsIndex, 1)
+//     questionCounter++
+//     progressText.innerText = questions (questionCounter); (MAX_QUESTIONS)
 
-    acceptingAnswers = true
-}
+//     var questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+//     currentQuestion = availableQuestions[questionsIndex]
+//     question.innerText = currentQuestion.question
 
-choices.forEach(choice => {
-    choice.addEventListener("click", event => {
-        if(!acceptingAnswers) return
+//     choices.forEach(choice => {
+//         var number = choice.dataset["number"]
+//         choice.innerText = currentQuestion["choice" + number]
+//     })
 
-        acceptingAnswers = false
-        var selectedChoice = event.target
-        var selectedAnswer = selectedChoice.dataset["number"]
+//     availableQuestions. splice(questionsIndex, 1)
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect"
+//     acceptingAnswers = true
+// }
 
-        if(classToApply === "correct") {
-            incrementscore(SCORE_POINTS)
-        }
+// choices.forEach(choice => {
+//     choice.addEventListener("click", event => {
+//         if(!acceptingAnswers) return
 
-        selectedChoice.parentElement.classList.add(classToApply)
+//         acceptingAnswers = false
+//         var selectedChoice = event.target
+//         var selectedAnswer = selectedChoice.dataset["number"]
 
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
+//         let classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect"
 
-        }, 1000)
-    })
-})
+//         if(classToApply === "correct") {
+//             incrementscore(SCORE_POINTS)
+//         }
 
-incrementScore = num => {
-    score += num
-    scoreText.innerText = score
-}
+//         selectedChoice.parentElement.classList.add(classToApply)
 
-startGame()
+//         setTimeout(() => {
+//             selectedChoice.parentElement.classList.remove(classToApply)
+//             getNewQuestion()
+
+//         }, 1000)
+//     })
+// })
+
+// incrementScore = num => {
+//     score += num
+//     scoreText.innerText = score
+// }
+
+// startGame()
