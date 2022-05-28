57bb99c2b1a3ec5a9a2fd63810d7c7535720f1d5
jessicawright822 <104212202+jessicawright822@users.noreply.github.com>
2022-05-28 16:52:09 -0400

    score calculated and entered through highscore.js; Back buton updated

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,9 +1,23 @@
 var highScore = document.querySelector("#highScore");
-var clear = document.querySelector("#clea");
+var clearBtn = document.querySelector("#clea");
 var goBack = document.querySelector("goBack");
 
-clear.addEventListener("click", function() {
-    localStorage.clear();
+clearBtn.addEventListener("click", function () {
+    localStorage.clearBtn();
     location.reload();
 });
 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !=null) {
+    for (var i = 0; i < allScores.length; i++) {
+        var newLi = document.createElement("li");
+        newLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(newLi);
+    }
+}
+
+goBack.addEventListener("click", function() {
+    window.location.replace("./index.html");
+})
